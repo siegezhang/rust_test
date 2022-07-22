6c515c9338fa65207f70b83c55c287f3c99dd6bd
zhangxiaosheng <z1178858918@qq.com>
2022-07-22 19:38:19 +0800

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
@@ -5,4 +8,9 @@ pub fn derive_builder(input: TokenStream) -> TokenStream {
     let _ = input;
 
     unimplemented!()
+}
+
+#[proc_macro]
+pub fn make_answer(_item: TokenStream) -> TokenStream {
+    "fn answer() -> u32 { 42 }".parse().unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,6 @@ macro_rules! map {
 
 
 
-
 fn main() {
     foo();
     bar();
@@ -86,7 +85,6 @@ fn main() {
     // Recall that blocks are expressions too!
     print_result!({
         let x = 1u32;
-
         x * x + 2 * x - 1
     });
 }
