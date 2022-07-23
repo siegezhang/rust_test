16cd020f5c9016126b9cb7cb5f99323eb7a5d57b
zhangxiaosheng <z1178858918@qq.com>
2022-07-24 00:28:32 +0800

    commit

--- learning/src/test2.rs
+++ learning/src/test2.rs
@@ -1,21 +1,16 @@
 #[cfg(test)]
-mod test2 {
-    use rbatis::{crud::CRUD, rbatis::Rbatis};
+mod test6 {
 
-    #[tokio::test]
-    async fn rbatis_conn() {
-        let rb = Rbatis::new();
-        ///连接数据库,自动判断驱动类型"mysql://*","postgres://*","sqlite://*","mssql://*"加载驱动   
-        rb.link("mysql://root:123456@localhost:3306/rust_demo")
-            .await
-            .unwrap();
-        ///自定义连接池参数。(可选)
-        // use crate::core::db::DBPoolOptions;
-        // let mut opt =DBPoolOptions::new();
-        // opt.max_connections=100;
-        // rb.link_opt("mysql://root:123456@localhost:3306/test",&opt).await.unwrap();
-
-        //启用日志输出，你也可以使用其他日志框架，这个不限定的
-        fast_log::init_log("requests.log", 1000, log::Level::Info, None, true);
+    #[test]
+    fn test_vector() {
+        //let mut v=Vec::new();
+        let mut v = vec![1, 2, 3, 5];
+        v.push(3);
+        let third = &v[2];
+        print!("third number is {}\n", third);
+        match v.get(2) {
+            Some(third) => print!("third number is {}\n",third),
+            None=>print!("there is no elements\n")
+        }
     }
 }
